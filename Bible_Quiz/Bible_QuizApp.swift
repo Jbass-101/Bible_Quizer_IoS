c0294d010551c8addf8c558e12cacf34d30c14ff
jbass-101 <junior07bj@gmail.comm>
2023-11-02 20:15:49 +0200

    Initial Commit

---
+++ Bible_Quiz/Bible_QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bible_QuizApp.swift
+//  Bible_Quiz
+//
+//  Created by Paulette Stevena on 2023/11/02.
+//
+
+import SwiftUI
+
+@main
+struct Bible_QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
