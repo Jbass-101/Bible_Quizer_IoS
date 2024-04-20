edeff269d0907cc5435beaf8a36fd9102e424666
jbass-101 <junior07bj@gmail.comm>
2024-04-20 14:41:39 +0200

    Get Auth User

--- Bible_Quiz/feat-auth/data-service/AuthDataService.swift
+++ Bible_Quiz/feat-auth/data-service/AuthDataService.swift
@@ -13,7 +13,13 @@ final class AuthDataService {
     static let shared = AuthDataService()
     private init(){}
     
-    
+    func getAuthUser() throws -> UserModel {
+        guard let user = Auth.auth().currentUser else {
+            throw URLError(.badServerResponse)
+        }
+        
+        return UserModel(user: user)
+    }
     
     func createUserByEmail(email: String, password: String) async throws -> UserModel {
         let result = try await Auth.auth().createUser(withEmail: email, password: password)
