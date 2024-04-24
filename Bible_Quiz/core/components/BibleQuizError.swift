adb6255ba3bbf5b154561e156eb4ca5786f0444a
jbass-101 <junior07bj@gmail.comm>
2024-04-24 23:08:47 +0200

    Default Bible Quiz Error

---
+++ Bible_Quiz/core/components/BibleQuizError.swift
@@ -0,0 +1,39 @@
+//
+//  BibleQuizError.swift
+//  Bible_Quiz
+//
+//  Created by Paulette Stevena on 2024/04/24.
+//
+
+import SwiftUI
+
+
+struct BibleQuizError: View {
+    
+    var errorString: String
+    var onDismiss: () -> Void
+    
+    var body: some View {
+        VStack{
+            Text(errorString)
+                .font(.callout)
+                .foregroundColor(.red)
+            
+            BibleQuizButton(title: "Home", onClick: {
+                onDismiss()
+            })
+        }
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.gray)
+//        .opacity(0.7)
+    }
+}
+
+struct BibleQuizError_Previews: PreviewProvider {
+    static var previews: some View {
+        BibleQuizError(errorString: "This is the error String"){
+            
+        }
+    }
+}
